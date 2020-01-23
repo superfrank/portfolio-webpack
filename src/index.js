bbeee54cbb6512ac8651adcd609f3f25b4dc0493
superfrank <frank.hulley-jones@guardian.co.uk>
2020-01-23 13:57:17 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+require("normalize.css/normalize.css");
+require("./styles/index.scss");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const pluginsTriggerElement = document.getElementById("plugins-trigger");
+    const pluginsElement = document.getElementById("plugins");
+
+    const pluginsVisibleClass = "splash-overview-plugins__list--visible";
+
+    pluginsTriggerElement.onclick = () => {
+        pluginsElement.classList.toggle(pluginsVisibleClass);
+    };
+});
