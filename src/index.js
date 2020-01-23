8c3738f13cfb41423e8abd28c75a3521771c8015
superfrank <frank.hulley-jones@guardian.co.uk>
2020-01-23 18:11:34 +0000

    playing around with anime-js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,19 @@ require("./styles/index.scss");
 document.addEventListener("DOMContentLoaded", () => {
     console.log("test");
 });
+
+window.addEventListener("load", function() {
+    var delay = 2;
+    var nodes = document.querySelectorAll(".animate");
+    for (var i = 0; i < nodes.length; i++) {
+        var words = nodes[i].innerText.split(" ");
+        nodes[i].innerHTML = "";
+        for (var i2 = 0; i2 < words.length; i2++) {
+            var item = document.createElement("span");
+            item.innerText = words[i2];
+            var calc = delay + (nodes.length + i2) / 3;
+            item.style.animationDelay = calc + "s";
+            nodes[i].appendChild(item);
+        }
+    }
+});
--- src/project/internet.js
+++ src/project/internet.js
@@ -1,3 +1,23 @@
 require("normalize.css/normalize.css");
 require("../styles/index.scss");
 console.log("test");
+import anime from "animejs";
+
+anime({
+    targets: ".svg__hand path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    duration: 3000
+    // direction: "alternate",
+    // endDelay: 2000,
+    // loop: true
+});
+
+anime({
+    targets: ".svg__hand",
+    keyframes: [{ translateX: -20 }, { translateX: 0 }],
+    delay: 3000,
+    duration: 1500,
+    easing: "spring(1, 80, 10, 0)",
+    loop: true
+});
