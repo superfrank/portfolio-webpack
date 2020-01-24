ded7a599ba37139d37178983b8e667f8ba154ace
superfrank <frank.hulley-jones@guardian.co.uk>
2020-01-24 12:51:19 +0000

    updated media links and added china video

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 require("normalize.css/normalize.css");
 require("./styles/index.scss");
 
-const myVideo = require("./assets/media/internetone.mp4");
+// const myVideo = require("./assets/media/internetone.mp4");
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("test");
