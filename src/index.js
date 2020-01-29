4d0adc5b9e0a7502c56bbc97ffeb187ca9879a7c
superfrank <frank.hulley-jones@guardian.co.uk>
2020-01-29 22:06:36 +0000

    big progress on hover image loading in

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ require("normalize.css/normalize.css");
 require("./styles/index.scss");
 import inView from "in-view";
 import anime from "animejs";
+import Marquee3k from "marquee3000";
+import TweenMax from "./assets/TweenMax.min.js";
+import imagesLoaded from "imagesloaded";
 
 // const myVideo = require("./assets/media/internetone.mp4");
 
@@ -22,6 +25,7 @@ inView(".animated-type h2")
     .on("enter", section => {
         // classList.add adds a class
         console.log("in-view");
+        Marquee3k.init();
         anime({
             targets: ".first",
             translateY: "8vw",
@@ -64,3 +68,202 @@ inView.threshold(1);
 //         }
 //     }
 // });
+
+// image hover //
+!(function(e) {
+    "undefined" == typeof module ? (this.charming = e) : (module.exports = e);
+})(function(e, n) {
+    "use strict";
+    n = n || {};
+    var t = n.tagName || "span",
+        o = null != n.classPrefix ? n.classPrefix : "char",
+        r = 1,
+        a = function(e) {
+            for (
+                var n = e.parentNode, a = e.nodeValue, c = a.length, l = -1;
+                ++l < c;
+
+            ) {
+                var d = document.createElement(t);
+                o && ((d.className = o + r), r++),
+                    d.appendChild(document.createTextNode(a[l])),
+                    n.insertBefore(d, e);
+            }
+            n.removeChild(e);
+        };
+    return (
+        (function c(e) {
+            for (
+                var n = [].slice.call(e.childNodes), t = n.length, o = -1;
+                ++o < t;
+
+            )
+                c(n[o]);
+            e.nodeType === Node.TEXT_NODE && a(e);
+        })(e),
+        e
+    );
+});
+
+{
+    const getMousePos = e => {
+        let posx = 0;
+        let posy = 0;
+        if (!e) e = window.event;
+        if (e.pageX || e.pageY) {
+            posx = e.pageX;
+            posy = e.pageY;
+        } else if (e.clientX || e.clientY) {
+            posx =
+                e.clientX +
+                document.body.scrollLeft +
+                document.documentElement.scrollLeft;
+            posy =
+                e.clientY +
+                document.body.scrollTop +
+                document.documentElement.scrollTop;
+        }
+        return { x: posx, y: posy };
+    };
+
+    // Effect 2
+    class HoverImgFx2 {
+        constructor(el) {
+            this.DOM = { el: el };
+            this.DOM.reveal = document.createElement("div");
+            this.DOM.reveal.className = "hover-reveal";
+            this.DOM.reveal.innerHTML = `<div class="hover-reveal__inner"><div class="hover-reveal__img" style="background-image:url(${this.DOM.el.dataset.img})"></div></div>`;
+            this.DOM.el.appendChild(this.DOM.reveal);
+            this.DOM.revealInner = this.DOM.reveal.querySelector(
+                ".hover-reveal__inner"
+            );
+            this.DOM.revealInner.style.overflow = "hidden";
+            this.DOM.revealImg = this.DOM.revealInner.querySelector(
+                ".hover-reveal__img"
+            );
+
+            this.initEvents();
+        }
+        initEvents() {
+            this.positionElement = ev => {
+                const mousePos = getMousePos(ev);
+                const docScrolls = {
+                    left:
+                        document.body.scrollLeft +
+                        document.documentElement.scrollLeft,
+                    top:
+                        document.body.scrollTop +
+                        document.documentElement.scrollTop
+                };
+                this.DOM.reveal.style.top = `${mousePos.y +
+                    20 -
+                    docScrolls.top}px`;
+                this.DOM.reveal.style.left = `${mousePos.x +
+                    20 -
+                    docScrolls.left}px`;
+            };
+            this.mouseenterFn = ev => {
+                this.positionElement(ev);
+                this.showImage();
+            };
+            this.mousemoveFn = ev =>
+                requestAnimationFrame(() => {
+                    this.positionElement(ev);
+                });
+            this.mouseleaveFn = () => {
+                this.hideImage();
+            };
+
+            this.DOM.el.addEventListener("mouseenter", this.mouseenterFn);
+            this.DOM.el.addEventListener("mousemove", this.mousemoveFn);
+            this.DOM.el.addEventListener("mouseleave", this.mouseleaveFn);
+        }
+        showImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    this.DOM.reveal.style.opacity = 1;
+                    TweenMax.set(this.DOM.el, { zIndex: 1000 });
+                }
+            })
+                .add("begin")
+                .add(
+                    new TweenMax(this.DOM.revealInner, 0.4, {
+                        ease: Quint.easeOut,
+                        startAt: { x: "-100%", y: "-100%" },
+                        x: "0%",
+                        y: "0%"
+                    }),
+                    "begin"
+                )
+                .add(
+                    new TweenMax(this.DOM.revealImg, 0.4, {
+                        ease: Quint.easeOut,
+                        startAt: { x: "100%", y: "100%" },
+                        x: "0%",
+                        y: "0%"
+                    }),
+                    "begin"
+                );
+        }
+        hideImage() {
+            TweenMax.killTweensOf(this.DOM.revealInner);
+            TweenMax.killTweensOf(this.DOM.revealImg);
+
+            this.tl = new TimelineMax({
+                onStart: () => {
+                    TweenMax.set(this.DOM.el, { zIndex: 999 });
+                },
+                onComplete: () => {
+                    TweenMax.set(this.DOM.el, { zIndex: "" });
+                    TweenMax.set(this.DOM.reveal, { opacity: 0 });
+                }
+            })
+                .add("begin")
+                .add(
+                    new TweenMax(this.DOM.revealInner, 0.3, {
+                        ease: Quint.easeOut,
+                        x: "100%",
+                        y: "100%"
+                    }),
+                    "begin"
+                )
+
+                .add(
+                    new TweenMax(this.DOM.revealImg, 0.3, {
+                        ease: Quint.easeOut,
+                        x: "-100%",
+                        y: "-100%"
+                    }),
+                    "begin"
+                );
+        }
+    }
+
+    [...document.querySelectorAll('[data-fx="2"] > a, a[data-fx="2"]')].forEach(
+        link => new HoverImgFx2(link)
+    );
+
+    // Demo purspose only: Preload all the images in the page..
+    const contentel = document.querySelector(".content");
+    [
+        ...document.querySelectorAll(
+            ".block__title, .block__link, .content__text-link"
+        )
+    ].forEach(el => {
+        const imgsArr = el.dataset.img.split(",");
+        for (let i = 0, len = imgsArr.length; i <= len - 1; ++i) {
+            const imgel = document.createElement("img");
+            imgel.style.visibility = "hidden";
+            imgel.style.width = 0;
+            imgel.src = imgsArr[i];
+            imgel.className = "preload";
+            contentel.appendChild(imgel);
+        }
+    });
+    imagesLoaded(document.querySelectorAll(".preload"), () =>
+        document.body.classList.remove("loading")
+    );
+}
