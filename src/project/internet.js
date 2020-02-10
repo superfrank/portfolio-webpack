d625a6067a740d3b2a5d34d10e1c3e1a79f151b8
superfrank <frank.hulley-jones@guardian.co.uk>
2020-02-10 23:18:29 +0000

    favicons added and link hand added on internet

--- src/project/internet.js
+++ src/project/internet.js
@@ -17,28 +17,33 @@ if (mqTwo.matches) {
         easing: "easeInOutSine",
         loop: true
     });
-
 } else {
     anime({
         targets: ".svg__hand--desktop",
         keyframes: [{ translateX: -18 }, { translateX: 0 }],
-        delay: 3000,
+        delay: 2000,
         duration: 1500,
         easing: "spring(1, 80, 10, 0)",
         loop: true
     });
 }
-
-
-
 anime({
-    targets: ".svg__arrow--link",
+    targets: ".svg__mickey--link",
     keyframes: [{ translateX: -8 }, { translateX: 0 }],
     delay: 2000,
     duration: 1500,
     easing: "spring(1, 80, 10, 0)",
     loop: true
 });
+
+// anime({
+//     targets: ".svg__arrow--link",
+//     keyframes: [{ translateX: -8 }, { translateX: 0 }],
+//     delay: 2000,
+//     duration: 1500,
+//     easing: "spring(1, 80, 10, 0)",
+//     loop: true
+// });
 
 // inView(".caption__header")
 //     .on("enter", section => {
